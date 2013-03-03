b07cb9a192c1eca6342d55a475bd927fb585f8e4
scottsavarie@gmail.com <158brunnenstrasse>
2013-03-03 18:50:19 +0100

    specialty module
    Signed-off-by: scottsavarie@gmail.com <158brunnenstrasse>

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,10 @@
 //= require "modules/log-polyfill.js"
 //= require "modules/modernizr.custom.js"
 //= require "modules/offcanvas.js"
+
+// $(document).ready(function(){
+//     $('#header-cover').css({'min-height':(($(window).height())-0)+'px'});
+//     $(window).resize(function(){
+//     $('#header-cover').css({'min-height':(($(window).height())-0)+'px'});
+//     });
+// });
