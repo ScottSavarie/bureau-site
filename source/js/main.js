e208c73e84de5625c7217bd06393f844ea7cdf14
scottsavarie@gmail.com <158brunnenstrasse>
2013-03-11 16:16:35 +0100

    Lots of fancy javascript animations
    Signed-off-by: scottsavarie@gmail.com <158brunnenstrasse>

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,72 @@
-//= require "modules/jquery-1.8.3.min.js"
-//= require "modules/log-polyfill.js"
-//= require "modules/modernizr.custom.js"
-//= require "modules/offcanvas.js"
 
-// $(document).ready(function(){
-//     $('#header-cover').css({'min-height':(($(window).height())-0)+'px'});
-//     $(window).resize(function(){
-//     $('#header-cover').css({'min-height':(($(window).height())-0)+'px'});
-//     });
+
+/* =Home fade ins
+============================================================================== */
+$(document).ready(function() {
+    $("#video-trigger, #home-arrow").hide(0);
+    $("#video-trigger, #home-arrow").delay(500).fadeIn('slow');
+});
+
+$(document).ready(function() {
+    $('body').hide();
+    $('body').delay(100).show('fast');
+});
+
+
+// $(document).ready(function() {
+//     var width = $(window).width();
+//     if (width >= 767) {
+//         $("#home-nav").hide();
+//         $("#home-nav").delay(200).slideDown('fast');
+//     }
+//     else {
+//         //Do Something Else
+//     }
 // });
+
+
+
+
+/* =Home arrow slide
+============================================================================== */
+
+$("#home-arrow").click(function(){
+    $('html,body').animate({scrollTop: jQuery("#specialty").offset().top - 100},'slow'); 
+
+});
+
+
+
+
+
+/* =Home specialty fade
+============================================================================== */
+
+$(document).ready(function() {
+    var width = $(window).width();
+    if (width >= 767) {
+        $("#specialty-one, #specialty-two, #specialty-three").hide();
+            
+
+            $(window).scroll(function(event) {
+            if($("#specialty").offset().top < $(window).scrollTop() + $(window).outerHeight()) {
+                $("#specialty-one").fadeIn(600);
+                $("#specialty-two").fadeIn(900);
+                $("#specialty-three").fadeIn(1100);
+            } else {
+                // do nothing
+            }
+            });
+    
+    }
+    else {
+        //Do Something Else
+    }
+
+
+
+    
+});
+
+
+
