89c283ff9a01c5417ac8f9af48236755acf24305
scottsavarie@gmail.com <158brunnenstrasse>
2013-03-25 10:19:27 +0100

    Enhance sticky nag with JS
    Signed-off-by: scottsavarie@gmail.com <158brunnenstrasse>

--- source/js/main.js
+++ source/js/main.js
@@ -59,6 +59,12 @@ $(document).ready(function() {
                 // do nothing
             }
             });
+
+
+                
+
+
+
     
     }
     else {
@@ -68,11 +74,52 @@ $(document).ready(function() {
 });
 
 
+/* =Home video swap
+============================================================================== */
+
 $("#videotrigger").click(function () { 
     $("#videotarget").attr("src", "http://www.youtube.com/embed/1sYNgnUaAyg?list=PL6NX9H_ihCfDqariNCYuP8Tqu3AfZ_Ik6&amp;title=0&amp;modestbranding=1&amp;showinfo=0&amp;byline=0&amp;portrait=0&amp;color=757778&amp;autoplay=1&amp;autohide=1&amp;controls=0&amp;start=2&amp;loop=1");
     $(this).hide(0);
 
 });
+
+
+
+/* =Sticky Nav enhance
+============================================================================== */
+
+
+        var lastScrollTop = 0;
+        $(window).scroll(function(event){
+           var st = $(this).scrollTop();
+           if (st > lastScrollTop){
+                    $("#nav-fixed").removeClass("visible");
+                    $("#nav-fixed").addClass("hidden"); 
+           } 
+
+           else {
+                    $("#nav-fixed").removeClass("hidden");  
+                    $("#nav-fixed").addClass("visible");
+           }
+           lastScrollTop = st;
+        });
+
+
+
+        $(window).scroll(function(event){
+           var st = $(this).scrollTop();
+           if (st <= 10){
+
+                    $("#nav-fixed").removeClass("hidden");  
+                    $("#nav-fixed").addClass("visible");
+
+           }
+
+           else{
+
+           }
+
+       });
 
 
 
