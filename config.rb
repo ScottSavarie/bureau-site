0bb58dd6ed5b02f3e65d11360a7e1ebf1be26f10
scottsavarie@gmail.com <158brunnenstrasse>
2013-07-08 21:59:38 +0200

    pretty urls, minify js and css
    Signed-off-by: scottsavarie@gmail.com <158brunnenstrasse>

--- config.rb
+++ config.rb
@@ -59,10 +59,10 @@ set :images_dir, 'img'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
@@ -77,6 +77,9 @@ configure :build do
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
+
+activate :directory_indexes
+  
 end
 
 
--- source/js/main.js
+++ source/js/main.js
@@ -147,43 +147,37 @@ $("#videotrigger").click(function () {
 ============================================================================== */
 
 
-function scrollFunc(e) {
-
-    if (window.pageYOffset < 0 || window.pageYOffset > (document.height - window.innerHeight)) return false;
-
-    if ( typeof scrollFunc.x == 'undefined' ) {
-        scrollFunc.x=window.pageXOffset;
-        scrollFunc.y=window.pageYOffset;
-    }
-    var diffX=scrollFunc.x-window.pageXOffset;
-    var diffY=scrollFunc.y-window.pageYOffset;
-
-    if( diffX<0 ) {
-        // Scroll right
-    } else if( diffX>0 ) {
-        // Scroll left
-    } else if( diffY<0 ) {
-        // Scroll down Article Nav show
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
             $("#nav-fixed").removeClass("visible");
             $("#nav-fixed").addClass("hidden"); 
+   } 
+
+   else {
+            $("#nav-fixed").removeClass("hidden");  
+            $("#nav-fixed").addClass("visible");
+   }
+   lastScrollTop = st;
+});
 
 
 
-    } else if( diffY>0 ) {
-        // Scroll up Main Nav Show
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st <= 10){
 
             $("#nav-fixed").removeClass("hidden");  
             $("#nav-fixed").addClass("visible");
 
-    } else {
-        // First scroll event
-    }
-    scrollFunc.x=window.pageXOffset;
-    scrollFunc.y=window.pageYOffset;
+   }
 
-}
-window.onscroll=scrollFunc
+   else{
 
+   }
+
+});
 
 
 
